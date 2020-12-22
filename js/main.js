a47706f53df288898699d605448a4b924ee7bc00
RODRIGO OLIVEIRA <rodrigo.oliveira85@fatec.sp.gov.br>
2020-12-22 11:52:17 -0300

    Inicio

---
+++ js/main.js
@@ -0,0 +1,101 @@
+var nome = "Rodrigo Storti de Oliveira";
+//var idade = 32;
+//var idade2 = 50;
+//var frase = "Tricolor é Lider";
+
+//var lista = ["maça", "pera", "laranja"];
+
+//var fruta = [
+//                {nome:"maça", cor:"vermelha"},                
+//                {nome:"uva", cor:"roxa"},
+           
+//            ];
+
+//alert("Nome: " + nome + ", tem: " + idade+" anos");
+//alert(idade + idade2);
+
+
+
+/*
+console.log(nome);
+console.log(idade);
+console.log(frase);
+console.log(frase.replace("tricolor","Brasil"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+
+console.log(lista);
+//adiciona mais um elemento no final
+lista.push("uva");
+
+//apaga o último elemeto.
+console.log(lista);
+lista.pop();
+console.log(lista);
+
+console.log(lista.length);
+
+//invertre os elementos
+console.log(lista.reverse());
+
+
+//trazer como string
+console.log(lista.toString());
+
+//primeiro elemento
+console.log(lista[0]);
+
+//primeira letra do primeiro elemento
+console.log(lista.toString()[0]);
+
+//muda o separador da lista
+console.log(lista.join(" - "))
+
+console.log(fruta);
+
+console.log(fruta[0].nome);
+console.log(fruta[0].cor);
+
+
+console.log(fruta[1].nome);
+console.log(fruta[1].cor);
+
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+
+if (idade >=18)
+{
+    alert("Maior de idade");    
+}else
+{
+    alert("Menor de idade");
+}
+*/
+
+/*
+var count = 0;
+while(count <= 5)
+{
+    console.log(count);
+    count ++;
+}
+*/
+
+/*
+var count;
+for (count = 0; count <=5; count ++)
+{
+    console.log(count);
+}
+*/
+
+var d = new Date();
+
+alert(d);
+alert(d.getMonth() + 1);
+alert(d.getDay());
+
+
